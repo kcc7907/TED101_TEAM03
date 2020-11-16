ff397ed7571eccc6f78bcc34d71a61196625f6d6
jk890213 <jk890213@gmail.com>
2020-11-16 11:05:27 +0800

    form送出判定

--- js/08/meminfo.js
+++ js/08/meminfo.js
@@ -8,8 +8,11 @@ $(document).ready(function(){
 
         let myself = e.target.className;
         if(myself === "btn_pwd"){   //修改密碼
-            $('#changePassword').css({display:'flex'}).slideDown();
-            $('#password').css({display:'none'});
+            $('#changePassword input').attr({value:''}); //初值為空字串
+            $('#changePassword').css({display:'flex'}).slideDown(); //顯示修改密碼欄位
+
+            $('#password').css({display:'none'});            
+
         }else{                      //修改一般資料
             let inputChange = $(`input#${myself}`);
             $(inputChange).attr({
@@ -22,16 +25,18 @@ $(document).ready(function(){
 
 
     //輸入時背景提醒
-    $('input').focus(function(){
+    
+    $('.input').focus(function(){
         $(this).css({
             backgroundColor: '#ddd',
         })
-    })
-    $('input').blur(function(){
+    });
+    $('.input').blur(function(){
         $(this).css({
             backgroundColor: 'rgba(0,0,0,0)',
         })
     });
+
 
     
     //限制手機僅能輸入數字
@@ -45,24 +50,25 @@ $(document).ready(function(){
     //以其他方式輸入非數字字元時取代
     $('#phone').keyup(function(e){
         let str = (e.target.value).replace(/\D/g, "");
-        //let str = e.target.value;
-        // 或是 let str = this.value;
         e.target.value = str;
     })
 
     //資料送出判定
     $('#memberInfo').submit(function(e){
-
+        
         let send_data = true;
 
         let email = document.getElementById("email");
-        // //信箱未填
-        // if(email.value == ""){
-        //     send_data = false;
-        //     email.addClass("-error");
-        // }else{
-        //     email.removeClass("-error");
-        // }
+         //信箱未填
+        if(email.value == ""){
+            send_data = false;
+            if(!(email.classList.contains('-error'))){                
+                $('label[for="email"]').after('<span style="color:red;">*修改欄位不可為空</sapn>')
+            }
+            $(email).addClass("-error");
+        }else{
+            $(email).removeClass("-error");
+        }
 
         //信箱格式
         // if(is.email(email.value)){
@@ -76,35 +82,59 @@ $(document).ready(function(){
         let phone = document.getElementById("phone");
         if(phone.value == ""){
             send_data = false;
-            phone.addClass("-error");
+            if(!(phone.classList.contains('-error'))){                
+                $('label[for="phone"]').after('<span style="color:red;">*修改欄位不可為空</sapn>')
+            }
+            $(phone).addClass("-error");
         }else{
-            phone.removeClass("-error");
+            $(phone).removeClass("-error");
         }
 
         //姓名未填
         let name = document.getElementById("name");
         if(name.value == ""){
             send_data = false;
-            name.addClass("-error");
+            if(!(name.classList.contains('-error'))){                
+                $('label[for="name"]').after('<span style="color:red;">*修改欄位不可為空</sapn>')
+            }
+            $(name).addClass("-error");
+            
         }else{
-            name.removeClass("-error");
+            $(name).removeClass("-error");
         }
 
         //地址未填
         let address = document.getElementById("address");
         if(address.value == ""){
             send_data = false;
-            address.addClass("-error");
+            if(!(address.classList.contains('-error'))){                
+                $('label[for="address"]').after('<span style="color:red;">*修改欄位不可為空</sapn>')
+            }
+            $(address).addClass("-error");
+
         }else{
-            address.removeClass("-error");
+            $(address).removeClass("-error");
         }
+
+        //密碼未填
+        let password = document.querySelectorAll('#changePassword input');
+        for(let i = 0; i < password.length; i++){
+
+            if(password[i].value == ""){
+                send_data = false;
+                // password[i].classList.add("-error");
+            }else{
+                // password[i].classList.remove("-error");
+            }
+        }
+        
 
         if(!send_data){
             e.preventDefault();
         }
     })
 
-
+    
 
 
 
