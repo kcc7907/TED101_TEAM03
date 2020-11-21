d65815e9c1d953ad3c8e66ac417ea1401f87f19d
JHOproject <yabe55230@gmail.com>
2020-11-21 17:45:38 +0800

    11/21 -2

---
+++ js/15/contest_nav_footer.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    $('.slideUp').click(function () {
+        $('footer').addClass('on').css('box-shadow','rgb(0,0,0,.5) 0 -0px 30px');
+        $(this).hide().css({
+            backgroundColor: '#CAC3BB',
+        });
+        $('.slideDn').show();
+    });
+
+    $('.slideDn').click(function () {
+        $('footer').removeClass('on').css('box-shadow','rgb(0,0,0,.5) 0 0 30px');
+        $(this).hide();
+        $('.slideUp').show();
+    });
+})
