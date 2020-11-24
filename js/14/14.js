b772968bb323b5ac7dcb47f70fb96b8dc7b28f6e
bomeibomei <janehuang0210@gmail.com>
2020-11-24 20:06:04 +0800

    1124

--- js/14/14.js
+++ js/14/14.js
@@ -62,34 +62,34 @@
 //     setInterval(movemove, 1000);
 // });
 
-風格切換
-let spanbtn = document.querySelectorAll('.stylelist li');
+// 風格切換
+// let spanbtn = document.querySelectorAll('.stylelist li');
 
-let flexjpg = document.getElementsByClassName('stylebox_right');
+// let flexjpg = document.getElementsByClassName('stylebox_right');
 
-for (let x = 0; x < spanbtn.length; x++) {
-    spanbtn[x].addEventListener('click', function () {
-        for (let y = 0; y < flexjpg.length; y++) {
+// for (let x = 0; x < spanbtn.length; x++) {
+//     spanbtn[x].addEventListener('click', function () {
+//         for (let y = 0; y < flexjpg.length; y++) {
 
-            flexjpg[y].style.display = 'none';
-            spanbtn[y].style.border = 'none';
-        }
-        flexjpg[x].style.display = 'inline-flex';
-        spanbtn[x].style.border = '1px solid #aca8a4';
-    });
-}
+//             flexjpg[y].style.display = 'none';
+//             spanbtn[y].style.border = 'none';
+//         }
+//         flexjpg[x].style.display = 'inline-flex';
+//         spanbtn[x].style.border = '1px solid #aca8a4';
+//     });
+// }
 
-頁籤
-$(function () {
-    $("a.tab").on("click", function (e) {
-        e.preventDefault();
+// 頁籤
+// $(function () {
+//     $("a.tab").on("click", function (e) {
+//         e.preventDefault();
 
-        /* 將頁籤列表移除所有 -on，再將指定的加上 -on */
-        $(this).closest("ul").find("a.tab").removeClass("-on");
-        $(this).addClass("-on");
+//         /* 將頁籤列表移除所有 -on，再將指定的加上 -on */
+//         $(this).closest("ul").find("a.tab").removeClass("-on");
+//         $(this).addClass("-on");
 
-        /* 找到對應的頁籤內容，加上 -on 來顯示 */
-        $("div.tab").removeClass("-on");
-        $("div.tab." + $(this).attr("data-target")).addClass("-on");
-    });
-});
+//         /* 找到對應的頁籤內容，加上 -on 來顯示 */
+//         $("div.tab").removeClass("-on");
+//         $("div.tab." + $(this).attr("data-target")).addClass("-on");
+//     });
+// });
