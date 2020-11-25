d7c3c8ed81e6cf3786fa77e4d629e4c03d0e20af
jk890213 <jk890213@gmail.com>
2020-11-25 22:18:31 +0800

    後台

--- js/08/authority.js
+++ js/08/authority.js
@@ -2,13 +2,19 @@ $(document).ready(function(){
 
     $('.jk_authority .menu .item').click(function(){
         
-        if($(this).hasClass('-open')){  //子項已展開
+        if($(this).hasClass('-open')){      //子項已展開
             $(this).parent('ul').children('li:not(.item)').slideUp();          //收合
-            $(this).removeClass('-open')   //移除展開記號
+            $(this).removeClass('-open');   //移除展開記號
+            $(this).children('i').css({     //箭頭轉回
+                transform: 'rotate(0deg)',
+            })
 
-        }else{                          //子項未展開
+        }else{                              //子項未展開
             $(this).parent('ul').children('li:not(.item)').slideDown();        //點擊展開
-            $(this).addClass('-open');  //給予展開記號
+            $(this).addClass('-open');      //給予展開記號
+            $(this).children('i').css({     //箭頭向下
+                transform: 'rotate(-90deg)',
+            })
             
 
         }
