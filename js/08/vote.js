3151d8f0ca3df5dc1da44aabaec135c13b511930
jk890213 <jk890213@gmail.com>
2020-11-26 21:40:01 +0800

    投票頁修正

--- js/08/vote.js
+++ js/08/vote.js
@@ -1,28 +1,17 @@
 $(document).ready(function(){
 
     //點擊詳細資訊
-    $('.jk_vote main .work .img').click(function(){  
+    $('.jk_vote main .work .img').click(function(){
+        let winWidth = $(window).width();
+        let winHeight = $(window).height();
         $('.jk_vote .backGround').css('display', 'flex');
+        
     });
 
     //關閉詳細資訊
     $('.jk_vote .clickVote .close').click(function(){  
         $('.jk_vote .backGround').css('display', 'none');
-    });
-    
-
-
-
-
-
+    }); 
 
 });
 
-function clickVote(){
-
-    $('.jk_vote main .work .img').click(function(){
-        alert();
-
-        // $('.jk_vote .backGround').css('display', 'flex');
-    });
-}
