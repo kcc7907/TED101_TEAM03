2d4c23e5c03ad5aa33ccc4aa99a1b330e267512e
JHOproject <yabe55230@gmail.com>
2020-12-04 21:36:59 +0800

    12/4 -1

---
+++ frontend/LoginR.php
@@ -0,0 +1,42 @@
+<?php
+    include("../php/utilClassJH.php");
+	$Util = new UtilClass();	
+
+    //建立SQL
+    $sql = "SELECT * FROM MEMBER WHERE MEM_ID = ? and MEM_PWD = ?";
+
+    //執行
+    $statement = $Util->getPDO()->prepare($sql);
+
+    //給值
+    $statement->bindValue(1, $_POST["account"]);
+    $statement->bindValue(2, $_POST["pwd"]);
+    $statement->execute();
+    $data = $statement->fetchAll();
+    // print_r($data);
+
+    $memberID = "";
+    $memberName = "";
+    foreach($data as $index => $row){
+        $memberID = $row["MEM_ID"];
+        $memberName = $row["MEM_NAME"];
+    }
+    // echo $memberID;
+    // echo $memberName;
+    //判斷是否有會員資料?
+    if($memberID != "" && $memberName != ""){
+        include("../php/memberClassJH.php");
+        $Member = new MemberClass();
+    
+        //將會員資訊寫入session
+        $Member->setMemberInfo($memberID, $memberName);
+
+        //導回產品頁        
+        echo "<script>alert('登入成功!'); location.href = 'home2D.php';</script>"; 
+    }else{
+        //跳出提示停留在登入頁
+        // echo "<script>alert('帳號或密碼錯誤!'); location.href = 'home2D.php';</script>";
+        echo "<script>alert('帳號或密碼錯誤!'); location.href = 'home2D.php';</script>"; 
+
+    }
+?>
--- js/15/home2D.js
+++ js/15/home2D.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // ===== 漢堡包 ======
+    // =================================== 漢堡包 ===================================
     let $_headerNav = $('header nav');
     $('.ham').click(function(){
         $('.ham span:first-child').toggleClass('-hamR');
@@ -9,79 +9,56 @@ $(document).ready(function () {
     });
 
 
-    // ===== 會員燈箱 =====
-    // *** <a>停止預設事件 ***
+    // =================================== 會員燈箱 ===================================
+    // <a>停止預設事件
     $('.icon a , a.signLink , a.logLink').click(function (e) {  
         e.preventDefault();
     });
 
-    // *** 隱藏表單 ***
+    // 隱藏表單
     $('div#login , div#signUp').hide();
 
-    // *** 點選「會員icon」出現「登入」表單 ***
-    $('div.icon a:first-child').click(function(){
-        $('div#login').show().css({
-            'z-index': '3',
-            'opacity': '1',
-            'top': '0',
-        });
-        // 動畫
-        setTimeout(() => {
-            logAni();
-        }, 500);
-    });
+    // 點選「會員icon」出現「登入」表單
+    $('div.icon a:first-child').click(log);
 
-    // *** 點選「註冊新會員」出現「註冊」表單 ***
-    $('a.signLink').click(function(){
-        // 登入隱藏
-        $('div#login').css({
-            'z-index': '-3',
-            'opacity': '0',
-            'top': '-30vh',
-        }).hide();
-        // 註冊顯現
-        $('div#signUp').show().css({
-            'z-index': '3',
-            'opacity': '1',
-            'top': '0',
-        });
-        // 動畫
-        setTimeout(() => {
-            signAni();
-        }, 500);
-    });
+    // 點選「註冊新會員」出現「註冊」表單
+    $('a.signLink').click(sign);
 
 
-    // *** 點選「回登入頁面」出現「登入」表單 ***
-    $('a.logLink').click(function(){
-        // 註冊隱藏
-        $('div#signUp').css({
-            'z-index': '-3',
-            'opacity': '0',
-            'top': '-30vh',
-        }).hide();
-        // 登入顯現
-        $('div#login').show().css({
-            'z-index': '3',
-            'opacity': '1',
-            'top': '0',
-        });
-        // 動畫
-        setTimeout(() => {
-            logAni();
-        }, 500);
-    });
+    // 點選「回登入頁面」出現「登入」表單
+    $('a.logLink').click(backToLog);
     
-    // *** 點選closeBtn關閉表單 ***
-    $('div.closebtn').click(function(){
-        $(this).parent('div').parent('div').css({
-            'z-index': '-3',
-            'opacity': '0',
-            'top': '-30vh',
-        }).hide();
-    });
+    // 點選closeBtn關閉表單
+    $('div.closebtn').click(closeLB);
+
+
+    // =================================== 登入判斷 ===================================
+    // $('button.login').click(function (e) {
+    //     let theThis = e;
+    //     $('.login :text').each(function () {  
+    //         if($(this).val() == ""){
+    //             $('form.login').preventDefault();
+    //             alert('no');
+    //             console.log(this);
+    //         }else{
+    //             alert('ok');
+    //         }
+    //     });
+    // });
+    // $('form.login').submit(function (e) { 
+    //     // e.preventDefault();
+    //     $('form.login :text, form.login :password').each(function () {  
+    //         if($(this).val() == ''){
+    //             e.preventDefault();
+    //             console.log(e);
+    //         }
+    //     });
+    // });
 
 });
+
+
+
 
 // ===== log in animation =====
 function logAni(){
@@ -95,4 +72,66 @@ function signAni(){
     let el = document.querySelector('#signUp');
     let myAnimation2 = new LazyLinePainter(el, { "ease": "easeOutExpo", "strokeWidth": 2, "strokeOpacity": 1, "strokeColor": "#865454", "strokeCap": "square" });
     myAnimation2.paint();
+}
+
+// ===== log in lightbox =====
+function log() {  
+    $('div#login').show().css({
+        'z-index': '3',
+        'opacity': '1',
+        'top': '0',
+    });
+    // 動畫
+    setTimeout(() => {
+        logAni();
+    }, 500);
+}
+
+// ===== sign up lightbox =====
+function sign() {  
+    // 登入隱藏
+    $('div#login').css({
+        'z-index': '-3',
+        'opacity': '0',
+        'top': '-30vh',
+    }).hide();
+    // 註冊顯現
+    $('div#signUp').show().css({
+        'z-index': '3',
+        'opacity': '1',
+        'top': '0',
+    });
+    // 動畫
+    setTimeout(() => {
+        signAni();
+    }, 500);
+}
+
+// ===== back to log in lightbox =====
+function backToLog() {  
+            // 註冊隱藏
+            $('div#signUp').css({
+                'z-index': '-3',
+                'opacity': '0',
+                'top': '-30vh',
+            }).hide();
+            // 登入顯現
+            $('div#login').show().css({
+                'z-index': '3',
+                'opacity': '1',
+                'top': '0',
+            });
+            // 動畫
+            setTimeout(() => {
+                logAni();
+            }, 500);
+}
+
+// ===== close lightbox =====
+function closeLB() {  
+    $(this).parent('div').parent('div').css({
+        'z-index': '-3',
+        'opacity': '0',
+        'top': '-30vh',
+    }).hide();
 }
