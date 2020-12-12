4b5651067b31a3a7dc2ed6399483f0cc72a42f23
bomeibomei <janehuang0210@gmail.com>
2020-12-12 16:24:22 +0800

    1212php

---
+++ frontend/case.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>傢俱賞 | 參考案例</title>
+    <link rel="stylesheet" href="../lib/reset.css">
+    <link rel="stylesheet" href="../css/14.css">
+    <link rel="Shortcut Icon" href="../img/favicon.ico">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+    <script src="../js/14/14.js"></script>
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+</head>
+
+<body>
+    <header class="common">
+        <div class="logo">
+            <a href="./home2D.html"><img src="../img/headerFooter/logo.svg"></a>
+        </div>
+        <nav>
+            <dav class="nav1">
+                <a href="./product.html">救世傢俱</a>
+                <a href="./case.html">參考案例</a>
+            </dav>
+            <dav class="nav2">
+                <a href="./qa.html">客戶服務</a>
+                <a href="./contest_participate.html">傢聚賞</a>
+            </dav>
+        </nav>
+        <div class="icon">
+            <a class="login" href="./meminfo.html">
+                <img src="../img/headerFooter/loginIcon.svg" alt="">
+            </a>
+            <a class="shop" href="./contest_main20.html">
+                <img src="../img/headerFooter/shoppingCart.svg" alt="">
+            </a>
+            <a class="ham">
+                <span></span>
+                <span></span>
+                <span></span>
+            </a>
+        </div>
+    </header>
+
+
+    <div id="JN">
+        <div class="introduce">
+            <div class="photo"></div>
+            <h1 class="slogan one titleFont1">“ 救世宅是一個專為人們設計的項目。”</h1>
+            <div class="intro">
+                <div class="introduce_detail">
+                    <div class="intro_detail">
+                        <div class="designer contentFont">王聖文 (SAM)</div>
+                        <div class="saying first contentFont lineHeight">
+                            以其高質量的作品而聞名，這使他成為建築領域的參考。
+                            鄭冠群（KCC）的建築物，飯店，商店，購物中心，夜總會，酒店和住宅幫助他在國際上樹立了自己的品牌。
+                            最令他困擾的是圍繞他的建築風格的一些理論，試圖使他適應特定的流派。
+                            對他來說，擁有特定且可識別的風格與失去驚喜元素是一樣的，這在他的練習中被認為是必不可少的。人類的幸福來自情感，這是突然產生的。
+                            確切知道某事將要發生完全中斷了這一刻。
+                        </div>
+                    </div>
+                    <div id="intro_sliderBoard">
+                        <ul id="pic_intro">
+                            <li><img src="../img/interiordesign/introduce/C-ChairDiningChair_GravityFloorLamp_GUBIDiningTable_MategotBowl_MategotFlowerPot_On.jpg"
+                                    class="intro_image"></li>
+                            <li><img src="../img/interiordesign/introduce/interior-design-ideas-MHouse-7_30_20-12.jpg"
+                                    class="intro_image"></li>
+                            <li><img src="../img/interiordesign/introduce/6c38934fed34b30522c9552ca605c441 (1).jpg"
+                                    class="intro_image"></li>
+                            <li><img src="../img/interiordesign/introduce/BatMeetingChair_TSDesk_BL2TableLamp_On.jpg"
+                                    class="intro_image"></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="designer_say">
+                    <img src="../img/interiordesign/introduce/1-140525_low.jpg" class="designer">
+                    <div class="saying second  contentFont">
+                        “我不是那種強加概念的建築師，只是想知道我是一個天才，而客戶卻不了解與建築有關的任何東西。我是相反的。我的所有客戶都為我的工作增添了一些寶貴的東西，這並不是偽善。”
+                    </div>
+                </div>
+            </div>
+            <div id="sliderBoard">
+                <ul id="content">
+                    <li><img src="../img/interiordesign/excellent/award/forest/geometric-patterns.jpg" class="image">
+                        <div class="other_text">
+                            <P class="other_text_award titleFont1">獎項紀錄</P>
+                            <p class="other_text contentFont">2020西班牙安德魯國際設計大賽沙發與木桌共效伴應-金獎</p>
+                            <p class="other_text contentFont">2020義大利國際設計大獎-檜木餐桌</p>
+                            <p class="other_text contentFont">2020紅點設計概念獎-檜木餐桌</p>
+                        </div>
+                    </li>
+                    <li><img src="../img/interiordesign/excellent/award/forest/collection-img1-1.jpg" class="image">
+                        <div class="other_text">
+                            <p class="other_text_award titleFont1">獎項紀錄</p>
+                            <p class="other_text contentFont"> 2018第八屆義大利國際設計大獎裝飾及家居用品設計獎 日出時光書桌－鉑金獎</p>
+                            <p class="other_text contentFont">2018西班牙第18屆安德魯國際設計大賽-晨曦辦公桌 銀獎</p>
+                        </div>
+                    </li>
+                    <li><img src="../img/interiordesign/excellent/award/forest/geometric-wall-tiles-interior-design.jpg"
+                            class="image">
+                        <div class="other_text">
+                            <p class="other_text_award titleFont1">獎項紀錄</p>
+                            <p class="other_text contentFont">2016紅點設計概念獎- 銀獎</p>
+                            <p class="other_text contentFont">美國International Design Award-多功能衣櫃-榮譽提名獎</p>
+                        </div>
+                    </li>
+                </ul>
+                <ul id="contentButton"></ul>
+            </div>
+        </div>
+        <div class="tab_container">
+            <div class="tab_list_block">
+                <ul class="tab_list">
+                    <li><a href="#" data-target="tab1" class="tab -on titleFont2">居家住宅</a></li>
+                    <li><a href="#" data-target="tab2" class="tab titleFont2">商業空間</a></li>
+                    <li><a href="#" data-target="tab3" class="tab titleFont2">辦公空間</a></li>
+                </ul>
+            </div>
+
+            <!-- 居家住宅 -->
+            <div class="tab tab1 -on">
+                <div class="stylebox_right">
+                    <!-- <div class="style_one_one"> -->
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case2.html" class="img_inter">
+                                <img src="../img/interiordesign/home/example/chang/C1569228155031.jpg" class="homepic">
+                                <span class="contentFont">信義</span>
+                                <span class="contentFont">徐公館</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case2.html" class="img_inter">
+                                <img src="../img/interiordesign/home/example/chu bai/C1569288447471.jpg"
+                                    class="homepic">
+                                <span class="contentFont">北投</span>
+                                <span class="contentFont">李公館</span>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case2.html" class="img_inter">
+                                <img src="../img/interiordesign/home/example/dan chang/C1569228045371.jpg"
+                                    class="homepic">
+                                <span class="contentFont">南港</span>
+                                <span class="contentFont">王公館</span>
+                            </a>
+                        </div>
+
+                    </div>
+                    <!-- </div> -->
+                    <!-- <div class="style_one_two"> -->
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case2.html" class="img_inter">
+                                <img src="../img/interiordesign/home/example/fu/C1569288783975.jpg" class="homepic">
+                                <span class="contentFont">松山</span>
+                                <span class="contentFont">林公館</span>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case2.html" class="img_inter">
+                                <img src="../img/interiordesign/home/example/lin/C1582619597131.jpg" class="homepic">
+                                <span class="contentFont">文山</span>
+                                <span class="contentFont">何公館</span>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case2.html" class="img_inter">
+                                <img src="../img/interiordesign/home/example/yo/C1569288522107.jpg" class="homepic">
+                                <span class="contentFont">內湖</span>
+                                <span class="contentFont">毛公館</span>
+                            </a>
+                        </div>
+
+                    </div>
+                    <!-- </div> -->
+                    <!-- <div class="style_one_three"> -->
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case2.html" class="img_inter">
+                                <img src="../img/interiordesign/home/example/kuo/C1594346104631.jpg" class="homepic">
+                                <span class="contentFont">中正</span>
+                                <span class="contentFont">紀公館</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case2.html" class="img_inter">
+                                <img src="../img/interiordesign/home/example/rezo/1493275936.jpg" class="homepic">
+                                <span class="contentFont">大安</span>
+                                <span class="contentFont">黃公館</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case2.html" class="img_inter">
+                                <img src="../img/interiordesign/home/example/yang/C1569225124301.jpg" class="homepic">
+                                <span class="contentFont">中山</span>
+                                <span class="contentFont">鄭公館</span>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- </div> -->
+                </div>
+            </div>
+
+
+            <!-- 商業空間 -->
+            <div class="tab tab2">
+                <div class="stylebox_right">
+                    <!-- <div class="style_two_one"> -->
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case3.html" class="img_inter">
+                                <img src="../img/interiordesign/commercial/example/love/mNkEs164hN.jpg"
+                                    class="commercial_pic">
+                                <span class="contentFont">台北</span>
+                                <span class="contentFont">Brun不然</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case3.html" class="img_inter">
+                                <img src="../img/interiordesign/commercial/example/chi/ZJXzuMkUTc.jpg"
+                                    class="commercial_pic">
+                                <span class="contentFont">首爾</span>
+                                <span class="contentFont">Sanet lind</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case3.html" class="img_inter">
+                                <img src="../img/interiordesign/commercial/example/coffee/_SKA6416_resize.jpg"
+                                    class="commercial_pic">
+                                <span class="contentFont">紐約</span>
+                                <span class="contentFont">Nsoe Sden </span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case3.html" class="img_inter">
+                                <img src="../img/interiordesign/commercial/example/da chiu/9xcvSYpQ6c.jpg"
+                                    class="commercial_pic">
+                                <span class="contentFont">曼谷</span>
+                                <span class="contentFont">Ihoeb Wgd</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case3.html" class="img_inter">
+                                <img src="../img/interiordesign/commercial/example/hung/8WiXIDl3nR.jpg"
+                                    class="commercial_pic">
+                                <span class="contentFont">東京</span>
+                                <span class="contentFont">Owf Sbwr Knn</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="jpgbox">
+                        <div class="inter_pic">
+                            <a href="./case3.html" class="img_inter">
+                                <img src="../img/interiordesign/commercial/example/xin/Q6V7nNahbr.jpg"
+                                    class="commercial_pic">
+                                <span class="contentFont">澳門</span>
+                                <span class="contentFont">Jofe Qfen</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- 辦公空間 -->
+            <div class="tab tab3">
+                <div class="stylebox_right">
+                    <div class="style_three_one">
+                        <div class="jpgbox">
+                            <div class="inter_pic">
+                                <a href="./case4.html" class="img_inter">
+                                    <img src="../img/interiordesign/office/example/chia/10-Scandinavian-iDiD-taoyuan 8_aef73.jpg"
+                                        class="office_pic">
+                                    <span class="contentFont">士林</span>
+                                    <span class="contentFont">紀公館</span></a>
+                            </div>
+                        </div>
+                        <div class="jpgbox">
+                            <div class="inter_pic">
+                                <a href="./case4.html" class="img_inter">
+                                    <img src="../img/interiordesign/office/example/ren/12Modern-iDiD-ntc 2_fe819.jpg"
+                                        class="office_pic">
+                                    <span class="contentFont">士林</span>
+                                    <span class="contentFont">紀公館</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="libft">
+        <section class="herf">
+            <div class="logo">
+                <img src="../img/headerFooter/logo.svg" alt="">
+            </div>
+            <div class="page">
+                <a href="./product.html">救世家具</a>
+                <a href="./case.html">優質案例</a>
+                <a href="./qa.html">客戶服務</a>
+                <a href="./qa.html">聯繫我們</a>
+                <a href="./contest_main20.html">傢俱賞</a>
+            </div>
+        </section>
+        <section class="link">
+            <div class="scb">
+                <label for="mail">訂閱電子報</label>
+                <input type="text" class="mail" placeholder="enter your email">
+            </div>
+            <div class="company">
+                <img src="../img/headerFooter/東和鋼鐵.png">
+                <img src="../img/headerFooter/cooperate_blum.png">
+                <img src="../img/headerFooter/foot_logo.png">
+            </div>
+        </section>
+        <section class="copy">
+            <p>Copy right 2020,All Right Reserved | By 救世宅</p>
+        </section>
+    </footer>
+    <script src="../lib/header/header.js"></script>
+    <!-- <script>
+        document.addEventListener('click', e => console.log(e.target))
+    </script> -->
+</body>
+
+</html>
