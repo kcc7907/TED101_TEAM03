376a0eee50e0516f5fc889b3c75a270e102b80e5
gbye <gbyewolf@gmail.com>
2020-12-14 08:02:36 +0800

    upade contest v.01 done, cart order js done

--- js/20/contest_main20.js
+++ js/20/contest_main20.js
@@ -3,6 +3,7 @@ let right =document.querySelector('.right');
 let slider =document.querySelector('.slider');
 let bgbox =document.querySelector('.bgbox');
 
+//mobile entry
 content.addEventListener('click',()=>{
     if(window.innerWidth<=800){
     content.classList.add('-none');
@@ -11,22 +12,144 @@ content.addEventListener('click',()=>{
     right.classList.add('-block');
     }
 });
+window.addEventListener('resize',()=>{
+    if(window.innerWidth>800){
+        content.classList.remove('-none');
+        right.classList.remove('-block');
+    }
+});
 
 
+//onClick change_content
 let rule_content = document.querySelectorAll('.rule_content');
 let tab_span = document.querySelectorAll('.rule_tab span');
+let px = parseInt(window.getComputedStyle(tab_span[1]).fontSize);
+tab_span[0].classList.add('onTarget');
+tab_span[0].style.setProperty('font-size',`${px+3}px`,"important");
 
 tab_span.forEach((x,y) => {
     tab_span[y].addEventListener('click',(e)=>{
         rule_content.forEach((a,b) => {
             rule_content[b].style.display = 'none';
+            tab_span[b].classList.remove('onTarget');
+            tab_span[b].style.setProperty('font-size',`${px}px`,"important");
         });
+        tab_span[y].classList.add('onTarget');
+        tab_span[y].style.setProperty('font-size',`${px+3}px`,"important");
         rule_content[y].style.display = 'block';
+        rule_content[y].scrollTop=0;
     });
 });
 
 
+//onScroll change_content
+let content_len =rule_content.length;
+rule_content.forEach((a,b) => {
+    a.addEventListener("scroll",()=>{
+        let scrollTotal = a.scrollHeight-a.clientHeight;
+        if(b===rule_content.length-1 && a.scrollTop === scrollTotal){
+            a.addEventListener('mousewheel',function wheelReset(e){
+                if(e.wheelDelta <= -120){
+                    rule_content[b].style.display = 'none';
+                    rule_content[0].style.display = 'block';
+                    rule_content[0].scrollTop=0;
+                    tab_span[0].classList.add('onTarget');
+                    tab_span[0].style.setProperty('font-size',`${px+3}px`,"important");
+                    tab_span[b].classList.remove('onTarget');
+                    tab_span[b].style.setProperty('font-size',`${px}px`,"important");
+                }
+            a.removeEventListener('mousewheel',wheelReset);
+            },{ passive: true});
+            rule_content[0].scrollTop=0;
+        }else if(a.scrollTop === scrollTotal){
+            a.addEventListener('mousewheel',function wheelNext(e){
+                if(e.wheelDelta <= -120){
+                    rule_content[b].style.display = 'none';
+                    rule_content[b+1].style.display = 'block';
+                    rule_content[b+1].scrollTop=0;
+                    tab_span[b+1].classList.add('onTarget');
+                    tab_span[b+1].style.setProperty('font-size',`${px+3}px`,"important");
+                    tab_span[b].classList.remove('onTarget');
+                    tab_span[b].style.setProperty('font-size',`${px}px`,"important");
+                }
+            a.removeEventListener('mousewheel',wheelNext);
+            },{ passive: true});
+        }
+    });
+});
+
+
+//footer
 $('.down img').click(function () {
     $(this).toggleClass('-scale');
     $('footer').toggleClass('-on').css('box-shadow', 'rgb(0,0,0,.5) 0 0 30px');
 });
+
+
+//slider
+let picbox = document.querySelector('.picbox');
+let logos = document.querySelectorAll('.picbox img');
+let leave = document.querySelector('.leave');
+
+let count=0;
+let picWidth = logos[0].offsetWidth;
+let len = logos.length;
+let num = picbox.offsetWidth/logos[0].offsetWidth;
+
+window.addEventListener('resize',getWidth);
+function getWidth(){
+    picWidth = logos[0].offsetWidth;
+    len = logos.length;
+    num = picbox.offsetWidth/logos[0].offsetWidth;
+}
+
+$('.arrowr img').click(function () {
+    if(parseInt($('.leave').css('left'))%picWidth == 0 ){
+        clearInterval(sliderRun);
+        if(num+count<len){
+            count++;
+            $('.leave').animate({
+                left:picWidth*count*-1
+            },{
+                complete: function () {
+                    sliderRun = setInterval(run, 2000);
+                }
+            })
+        }else if(num+count===len){
+            sliderRun = setInterval(run, 2000);
+        }
+    }
+});
+
+$('.arrowl img').click(function () {
+    if(parseInt($('.leave').css('left'))%picWidth == 0 ){
+        clearInterval(sliderRun);
+        if(count>0){
+            count--;
+            $('.leave').animate({
+                left:picWidth*count*-1
+            },{
+                complete: function () {
+                    sliderRun = setInterval(run, 2000);
+                }
+            });
+        }else if(count===0){
+            sliderRun = setInterval(run, 2000);
+        }
+    }
+});
+
+let sliderRun = setInterval(run, 2000);
+function run(){
+    if(num+count<len){
+        count++;
+        $('.leave').animate({
+            left:picWidth*count*-1
+        });
+    }else {
+        count=0;
+        $('.leave').animate({
+            left:picWidth*count*-1
+        });
+    }
+}
--- php/20/sendData.php
+++ php/20/sendData.php
@@ -1,38 +1,64 @@
 <?php
-    $db_host = "localhost";
-    $db_user = "team03";
-    $db_pass = "ted101g3";
-    $db_select = "team03";
-    $dbconnect = "mysql:host=".$db_host.";dbname=".$db_select;
-    $pdo = new PDO($dbconnect, $db_user, $db_pass);
+        $db_host = "localhost";
+        $db_user = "team03";
+        $db_pass = "ted101g3";
+        $db_select = "team03";
+        $dbconnect = "mysql:host=".$db_host.";dbname=".$db_select;
+        $pdo = new PDO($dbconnect, $db_user, $db_pass);
 
-    // $sql="SELECT `ORD_ID` FROM `order` ORDER BY `ORD_ID` DESC LIMIT 1";
-    $sql="SELECT COUNT(`ORD_ID`) FROM `ORDER`";
+        $rp = json_decode(file_get_contents('php://input'), true);
+        // $rp[fname] => 柏美玲
+        // $rp[memid] => A111200001
+        // $rp[fphone] => 0911111222
+        // $rp[fcity] => 高雄市
+        // $rp[fadd] => 中正路10號
+        // $rp[payment] => 未付款
+        // $rp[total] => 97300
+        // $rp[discount] => -3000
+        // $rp[products] => Array
 
-    $result = $pdo->query($sql);
-    $result->execute();
-    // $data = $result->fetchAll(PDO::FETCH_ASSOC);
-    $data = $result->fetchAll();
-    // echo json_encode($data);
-    // echo $data;
-    // print_r($rp)
-    // echo date("Y/m/d H:i:s");
+        //part1 insert delivery
+        $insert_delivery="INSERT INTO `delivery` ( `DEL_RECEIVER`, `DEL_CITY`, `DEL_ADDRESS`) VALUES (?,?,?)";
+        $statement = $pdo->prepare($insert_delivery);
+        $statement->bindValue(1,$rp['fname']);
+        $statement->bindValue(2,$rp['fcity']);
+        $statement->bindValue(3,$rp['fadd']);
+        $statement->execute();
 
-    // foreach($data as $index => $row){
-        // $num = trim($row[$index], $row[$index]);
-        // $num = strchr($row[$index], trim($row[$index],'A'));
-    // }
-    // echo trim($row[$index],'A');
+        //part2 insert order
+        $insert_order="INSERT INTO `ORDER` (`ORD_ID`, `ORD_PAYMENT`, `ORD_DATE`, `MEMBER_ID`, `DELIVERY_ID`, `ORD_BUY`, `ORD_PAY`,`ORD_DICOUNT`) VALUES (?,?,?,?,?,?,?,?)";
+        $sql="SELECT COUNT(`ORD_ID`) FROM `ORDER`";
+        $result = $pdo->query($sql);
+        $result -> execute();
+        $order_total = $result->fetchColumn();
+        $ymd = date("Y/m/d");
+        $date = date("Y/m/d H:i:s");
+        if($rp['payment'] == '未付款'){
+            $order_buy =$date;
+            $order_pay = null;
+        }else{
+            $order_buy =$date;
+            $order_pay = $date;
+        };
+        $statement = $pdo->prepare($insert_order);
+        $statement->bindValue(1,"A000".($order_total+1));
+        $statement->bindValue(2,$rp['payment']);
+        $statement->bindValue(3,$ymd);
+        $statement->bindValue(4,$rp['memid']);
+        $statement->bindValue(5,$order_total+1);
+        $statement->bindValue(6,$order_buy);
+        $statement->bindValue(7,$order_pay);
+        $statement->bindValue(8,$rp['discount']==0?null:$rp['discount']);
+        $statement->execute();
 
-    // foreach($data as $index => $row)
-    // echo $row[$index];
+        //part3 insert order_detail
+        $insert_detail="INSERT INTO `order_detail` ( `ORDER_ID`, `PRODUCT_ID`, `ORDER_QUANTITY`) VALUES ( ?,?,?)";
+        foreach($rp['products'] as $index => $row){
+            $statement = $pdo->prepare($insert_detail);
+            $statement->bindValue(1,"A000".($order_total+1));
+            $statement->bindValue(2,$row['prd_id']);
+            $statement->bindValue(3,$row['num']);
+            $statement->execute();
+        }
 
-
-    $rp = json_decode(file_get_contents('php://input'), true);
-    // foreach($rp as $index => $row){
-    //     echo $row;
-    // }
-    // print_r($rp);
-    echo $rp[0];
-    
 ?>
