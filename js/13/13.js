06f2f471efc0590a5f18bf9064263aababa8afa4
kcc7907 <kcc7907@gmail.com>
2020-12-18 21:53:43 +0800

    by kcc add backend insert

--- js/13/13.js
+++ js/13/13.js
@@ -3,8 +3,25 @@ let app = new Vue({
     el:'#app',
     data:{
         rp:'',
+        formData:{
+            mem_num:'',
+            mem_pwd: '',
+            mem_name: '',
+            mem_phone: '',
+            mem_email: '',
+            mem_city: '',
+            mem_addr: '',
+            mem_date: ''
+        }
     },
     methods: {
+        send(){
+            console.log(this.formData);
+            axios.post('../php/13/kc_memberInsert.php',this.formData).then(res => {
+                this.rp = res.data;
+            });
+            $('div.kcc_jump').addClass('-opacity-zero');
+        }
     },
     mounted() {
         axios.post('../php/13/kc_getMember.php').then(res => {
@@ -13,30 +30,30 @@ let app = new Vue({
     },
 });
 
-var btn = document.querySelector('#product_submit');
+// var btn = document.querySelector('#product_submit');
 
-btn.addEventListener('click',()=>{
-    let num = document.querySelector('#mem_num').value;
-    let pwd = document.querySelector('#mem_pwd').value;
-    let mname = document.querySelector('#mem_name').value;
-    let phone = document.querySelector('#mem_phone').value;
-    let email = document.querySelector('#mem_email').value;
-    let city = document.querySelector('#mem_city').value;
-    let addr = document.querySelector('#mem_addr').value;
-    let mdate = document.querySelector('#mem_date').value;
+// btn.addEventListener('click',()=>{
+//     let num = document.querySelector('#mem_num').value;
+//     let pwd = document.querySelector('#mem_pwd').value;
+//     let mname = document.querySelector('#mem_name').value;
+//     let phone = document.querySelector('#mem_phone').value;
+//     let email = document.querySelector('#mem_email').value;
+//     let city = document.querySelector('#mem_city').value;
+//     let addr = document.querySelector('#mem_addr').value;
+//     let mdate = document.querySelector('#mem_date').value;
 
-    let obj = {
-        num:num,
-        pwd:pwd,
-        mname: mname,
-    }
+//     let obj = {
+//         num:num,
+//         pwd:pwd,
+//         mname: mname,
+//     }
 
-    axios.post('../php/13/kc_memberInsert.php',obj).then(res => {
-        console.log(res.data);
-    });
+//     axios.post('../php/13/kc_memberInsert.php',obj).then(res => {
+//         console.log(res.data);
+//     });
 
 
-});
+// });
 
 
 
