94cafcdbf32e1f50367341e6047f4f4834b4d7b3
gbye <gbyewolf@gmail.com>
2020-12-21 06:43:17 +0800

    by kc update index v1.0

---
+++ php/13/kc_bk.php
@@ -0,0 +1,21 @@
+<?php
+    include("../connect.php");
+    $sql="SELECT * FROM `manager` WHERE `MGR_ACCOUNT` = ?";
+    $rp = json_decode(file_get_contents('php://input'), true);
+    $pwd = $rp['pwd'];
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(1,$rp['user']);
+    $statement->execute();
+    $data = $statement->fetchAll();
+    foreach($data as $index => $row){
+        $mid = $row["MGR_ACCOUNT"];
+        $mpwd = $row["MGR_PWD"];
+    }
+    if(!isset($mid)){
+        echo 'empty';
+    }else if ($pwd == $mpwd){
+        echo 'ok';
+    }else{
+        echo 'no';
+    }
+?>
