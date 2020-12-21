b7daa6a0eb0131213fe65be93ef54a47a85caf69
jk890213 <jk890213@gmail.com>
2020-12-21 20:54:03 +0800

    投票分頁修正

--- php/08/jk_clickPage.php
+++ php/08/jk_clickPage.php
@@ -19,7 +19,7 @@
 
         echo '<div class="work">';
             echo '<div class="img '.$row["WK_ID"].'" data-id="'.$row["WK_ID"].'">';
-            echo '<img src="'.$row["WK_IMG"].'">';
+            echo '<img src="'.$row["WK_DRAW"].'">';
             echo '<div class="get_num">得票數: <span>'.$row["WK_VOTES"].'</span></div>';                     
         echo '</div>
                 <p>
