326dd693b391eb24fcd69b3c5793f083259f0fd8
jk890213 <jk890213@gmail.com>
2020-12-21 15:58:30 +0800

    連線資料

--- php/08/jk_link.php
+++ php/08/jk_link.php
@@ -1,7 +1,13 @@
 <?php
         //MySQL相關資訊
 
-        include('../connect.php');
+        // include('../connect.php');
+        $db_host = "localhost";
+        $db_user = "team03";
+        $db_pass = "ted101g3";
+        $db_select = "team03";
+        $dbconnect = "mysql:host=".$db_host.";dbname=".$db_select;
+        $pdo = new PDO($dbconnect, $db_user, $db_pass);
 
         // $db_host = "localhost";
         // $db_user = "root";
