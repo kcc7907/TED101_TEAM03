6fc872908b8ab9c7a55d9695ffdb0fd15f9c2aff
SamWang231654 <sam231654@hotmail.com>
2020-12-24 19:38:15 +0800

    202012241940

--- js/13/cabinetOrder.js
+++ js/13/cabinetOrder.js
@@ -1,3 +1,59 @@
+
+
+// function validate() {
+
+//     let emailId = document.myForm.useremail.value;
+//     atpos = emailId.indexOf('@');
+//     dotpos = emailId.lastIndexOf('.');
+//     let phoneId = document.myForm.userphone.value;
+//     let phoneConfirm = /^[09]{2}\d{8}$/;
+//     // let phoneConfirm = /^09\d{2}-\d{6}$/;
+
+//     if (document.myForm.username.value == "") {
+//         alert("請輸入姓名!");
+//         document.myForm.username.focus();
+//         return false;
+//     }
+//     if (document.myForm.useremail.value == "") {
+//         alert("請輸入信箱!");
+//         document.myForm.useremail.focus();
+//         return false;
+//     }
+//     if (document.myForm.userphone.value == "") {
+//         alert("請輸入電話!");
+//         document.myForm.userphone.focus();
+//         return false;
+//     }
+//     if (document.myForm.useraddress.value == "") {
+//         alert("請輸入地址!");
+//         document.myForm.useraddress.focus();
+//         return false;
+//     }
+//     if (atpos < 1 || (dotpos - atpos < 2)) {
+//         alert('請輸入正確信箱格式- XXXXXX@XXXXX.com');
+//         document.myForm.useremail.focus();
+//         return false;
+//     }
+//     if (!phoneConfirm.test(phoneId)) {
+//         alert('請輸入正確電話格式- 09XXXXXXXX');
+//         document.myForm.userphone.focus();
+//         return false;
+//     }
+//     /////////////////////send email///////////////////////
+//     Email.send({
+//         SecureToken: "b67cba3c-c13e-4c1a-bbaf-3ce0f4a2c87e",
+//         // Username: "tibame.team3@gmail.com",
+//         // Password: "Tibame@group39494",
+//         To: 'tibame.team3@gmail.com',
+//         From: "tibame.team3@gmail.com",
+//         Subject: "This is the subject TEXT9494",
+//         Body: "And this is the body"
+//     }).then(
+//         message => alert('已收到您的資訊，感謝。')
+//     );
+// };
+
+
 $(document).ready(function () {
     $('#small01').on('click', function () {
         $('div.blackBack').addClass('-on');
@@ -36,6 +92,9 @@ $(document).ready(function () {
     });
 
 
+
+
+
     ///////////////////表單驗證-focus blur 效果///////////////////////
     $('#username').focus(function () {
         $(this).css("borderBottom", "3px solid #865454");
@@ -69,44 +128,5 @@ $(document).ready(function () {
 
 });
 
-function validate(){
 
-    let emailId = document.myForm.useremail.value;
-        atpos = emailId.indexOf('@');
-        dotpos = emailId.lastIndexOf('.');
-    let phoneId = document.myForm.userphone.value;
-    let phoneConfirm = /^[09]{2}\d{8}$/ ;
-    // let phoneConfirm = /^09\d{2}-\d{6}$/;
-
-    if( document.myForm.username.value == "" ){
-        alert( "請輸入姓名!" );
-        document.myForm.username.focus() ;
-        return false;
-    }
-    if( document.myForm.useremail.value == "" ){
-        alert( "請輸入信箱!" );
-        document.myForm.useremail.focus() ;
-        return false;
-    }
-    if( document.myForm.userphone.value == "" ){
-        alert( "請輸入電話!" );
-        document.myForm.userphone.focus() ;
-        return false;
-    }
-    if( document.myForm.useraddress.value == "" ){
-        alert( "請輸入地址!" );
-        document.myForm.useraddress.focus() ;
-        return false;
-    }
-    if(atpos <1 || (dotpos - atpos <2)){
-        alert('請輸入正確信箱格式- XXXXXX@XXXXX.com');
-        document.myForm.useremail.focus();
-        return false;
-    }
-    if(!phoneConfirm.test(phoneId)){
-        alert('請輸入正確電話格式- 09XXXXXXXX');
-        document.myForm.userphone.focus();
-        return false;
-    }
-};
 
