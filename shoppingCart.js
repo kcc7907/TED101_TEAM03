12135474d04ae2ae0dbf2c696609a71a28a20dbd
gbye <gbyewolf@gmail.com>
2021-01-07 17:16:01 +0800

    add for minify

---
+++ shoppingCart.js
@@ -0,0 +1,359 @@
+let delAll= document.querySelector('.delAll');
+let checkAll= document.querySelector('.checkAll');
+let title= document.querySelectorAll('.title');
+title[0].classList.add('-onColor');
+window.addEventListener('storage',()=>location.reload());
+
+let right = new Vue({
+    el:'#right',
+    data:{
+        total:0,
+        discount:0,
+        Shipping:0,
+        final:0,
+        proTotal:0
+    },
+    methods: {
+        backShop(){
+            pop.text = '是否返回商城?';
+            pop.show = true;
+            pop.confirm = 'backShop';
+        },
+        loginCheck(){
+            if(this.proTotal==0){
+                pop.text = '請先購買商品';
+                pop.show = true;
+            }else if(checkCookie('loging')){
+                pop.text = '是否確定購買?';
+                pop.show = true;
+                pop.confirm = 'buy';
+            }else{
+                $('div#login').show().css('zIndex', '4');
+                    $('div#login div.login').css({
+                    'opacity':'1',
+                    'top': '0',
+                });
+                setTimeout(() => {
+                    logAni();
+                }, 500);
+                (function memBox() {
+                    $('.icon >a , a.signLink').click(function (e) {
+                        e.preventDefault();
+                    });
+                    $('a.signLink').click(function(){
+                        signBox();
+                        $('form input').css({
+                            'backgroundColor': 'rgba(0, 0, 0, 0)',
+                            'border': 'none',
+                            'borderBottom': '1px solid #BDA79E',
+                        });
+                        $('form select').css('border' , '1px solid #BDA79E');
+                        $('#alivebox').removeClass('bcgColor');
+                    });
+                    $('a.logLink').click(function(e){
+                        backToLog();
+                        $('form input').css({
+                            'backgroundColor': 'rgba(0, 0, 0, 0)',
+                            'border': 'none',
+                            'borderBottom': '1px solid #BDA79E',
+                        });
+                        $('form select').css('border' , '1px solid #BDA79E');
+                    });
+                    $('div.closebtn').click(function(e){
+                        let thisBtn = e.target;
+                        closeLB(thisBtn);
+                    });
+                })();
+            }
+        },
+        buy(){
+            localStorage.removeItem("lists");
+            let buy = document.querySelectorAll('.product li');
+            let lists=[];
+            buy.forEach((a,b) => {
+                let obj ={
+                    prd_id: a.getAttribute('data-id'),
+                    num: a.getAttribute('data-num'),
+                    prd_price: a.getAttribute('data-price'),
+                    prd_name: a.getAttribute('data-name')
+                }
+                lists.push(obj);
+            });
+            localStorage.setItem("lists", JSON.stringify(lists));
+            localStorage.setItem("discount", this.discount);
+            location.href ="shoppingOrder.php";
+        },
+    },
+    watch: {
+        total(){ this.final = this.total+this.discount}
+    },
+    mounted() {
+    },
+});
+
+let all = new Vue({
+    el:'#all',
+    data:{
+    },
+    methods: {
+        delCheck(){
+            pop.text = '是否刪除選擇商品?';
+            pop.show = true;
+            pop.confirm = 'deleteChecked';
+        },
+        deleteAll(e){
+            let pick = document.querySelectorAll('.pick');
+            let lists=[];
+            pick.forEach((a,b) => {
+                if(a.checked===true){
+                    right.total-= a.closest('li').getAttribute('data-price')*a.closest('li').getAttribute('data-num');
+                    a.closest('ul').removeChild(a.closest('li'));
+                    right.proTotal -=a.closest('li').getAttribute('data-num');
+                }else{
+                    let obj ={
+                    prd_id: a.closest('li').getAttribute('data-id'),
+                    num: a.closest('li').getAttribute('data-num'),
+                    }
+                    lists.push(obj)
+                }
+            });
+            localStorage.removeItem("lists");
+            localStorage.setItem("lists", JSON.stringify(lists));
+
+            //購物車數量
+            let shoplist = JSON.parse(localStorage.getItem("lists"));
+            let shopIcon = document.querySelector('.shop');
+            let shopnum = 0;
+            shoplist.forEach(e => {
+                shopnum += parseInt(e.num);
+            });
+            if (shopnum === 0) {
+                return false;
+            } else if (!shopIcon.classList.contains('shopnum')) {
+                shopIcon.setAttribute('data-content', shopnum);
+                shopIcon.classList.add('shopnum');
+            } else {
+                shopIcon.setAttribute('data-content', shopnum);
+            }
+        },
+        checkAll(e){
+            e.target.checked==true?left.check=true:left.check=false;
+        },
+    },
+});
+
+let left = new Vue({
+    el:'#list',
+    data:{
+        check:false,
+        prdid:[],
+        rp:[],
+        proTotal:0,
+        discount_list:[],
+        discount_inner:[],
+        discount_specie:[],
+        disA:false,
+        disB:false,
+        disShow:false,
+        disArrA:[],
+        disArrB:[]
+    },
+    // mounted() {
+    created() {
+        let list = JSON.parse(localStorage.getItem("lists"));
+        if(!list || list.length === 0){
+            let list = document.querySelector('.left');
+            list.style.borderBottom = '1px solid #465d4c'
+            return;
+        }
+        list.forEach((a, b) => {
+            this.prdid.push(a.prd_id);
+            right.proTotal += parseInt(a.num);
+        });
+        //product
+        axios.post('../php/20/getProduct.php', this.prdid).then(res => {
+            this.rp = res.data;
+        }).catch(err=>{
+            if (err.response) {
+                console.log(err.response.status)
+            } else if (err.request) {
+                console.log(err.request)
+            } else {
+                console.log('Error', err.message)
+            }
+        });
+        //discount
+        axios.post('../php/20/getDiscount.php')
+        .then(res => {
+            this.discount_list = res.data;
+            this.discount_list.forEach((a,b) => {
+                this.discount_specie.push(a.DIS_ID);
+                this.discount_inner.push(a.DIS_PRODUCT_ID1);
+                this.discount_inner.push(a.DIS_PRODUCT_ID2);
+            });
+        }).then(res => {
+            let sp1, sp2;
+            this.discount_inner.forEach((a, b) => {
+                let getc = this.rp.filter((q, w) => {
+                    return q.PRD_NAME == a;
+                })
+                if (getc.length) {
+                    if (!sp1) {
+                        sp1 = getc[0].DISCOUNT_ID;
+                    } else if (sp1 === getc[0].DISCOUNT_ID) {
+                        sp1 += getc[0].DISCOUNT_ID;
+                    } else if (!sp2) {
+                        sp2 = getc[0].DISCOUNT_ID;
+                    } else { sp2 += getc[0].DISCOUNT_ID; }
+                }
+            });
+            if (sp1) {
+                if (sp1.length > 1) this.disA = true;
+                this.disArrA = sp1;
+            }
+            if (sp2) {
+                if (sp2.length > 1) this.disB = true;
+                this.disArrB = sp2;
+            }
+        })
+    },
+    watch: {
+        disA(){
+            if(this.disA==true)right.discount -= 500;
+            else right.discount +=500;
+        },
+        disB(){
+            if(this.disB==true)right.discount -= 2500;
+            else right.discount +=2500;
+
+            right.final = right.total+right.discount;
+        },
+        disArrA(){
+            if(this.disArrA.length<1)this.disA=false;
+        },
+        disArrB(){
+            if(this.disArrB.length<1)this.disB=false;
+        },
+    },
+});
+
+Vue.component('myList', {
+    data() {
+        return {
+            num: 1,
+        };
+    },
+    props: ['mycheck', 'prdId', 'url', 'prdName', 'prdPrice', 'discountId', 'prdMtl','myindex'],
+    template:
+        `<li class="list" :data-id="prdId" :data-price="prdPrice" :data-num="num" :data-dis="discountId"　:data-name="prdName">
+                <input type="checkbox" class="pick" :checked="mycheck">
+                <div class="picbox">
+                    <div class="imgbox">
+                        <img :src="url" class="pic">
+                        <div class="tag" v-show="discountId">
+                            <p>{{discountId}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="discountbox">
+                    <div class="discount" v-show="discountId">
+                        <div class="tag">
+                            <p>{{discountId}}</p>
+                        </div>
+                        <p>{{discountId=='A'?'狂熱桌椅組':'跳樓桌椅組'}}</p>
+                    </div>
+                </div>
+                <div class="info">
+                    <div class="procduct_name" >
+                        <p>{{prdName}}</p>
+                    </div>
+                    <div class="material" v-show="prdMtl">
+                        <p>黑色</p>
+                    </div>
+                    <div class="pricebox">
+                        <p class="dollar">$</p>
+                        <p class="price">{{prdPrice}}</p>
+                    </div>
+                </div>
+                <div class="count">
+                    <div class="totalbox">
+                        <p class="dollar">$</p>
+                        <p class="price_total">{{prdPrice}}</p>
+                    </div>
+                    <div class="numbox">
+                        <div class="arrowl"><img src="../img/shoppincart/left-G.svg" alt="" @click='btnl'></div>
+                        <input type="number" class="num" :value="num" disabled>
+                        <div class="arrowr"><img src="../img/shoppincart/right-G.svg" alt="" @click='btnr'>
+                        </div>
+                    </div>
+                </div>
+                <img src="../img/shoppincart/trash.svg" alt="" class="trash" @click='deleteList'>
+            </li>`,
+    methods: {
+        btnl(e) {
+            if(this.num > 1){
+                this.num--;
+                right.total -= parseInt(this.prdPrice);
+                right.proTotal--;
+            }
+        },
+        btnr() {
+            this.num++;
+            right.total += parseInt(this.prdPrice) ;
+            right.proTotal++;
+        },
+        deleteList(e) {
+            this.$el.classList.add('slide-fade-leave-active');
+            this.$el.classList.add('slide-fade-leave-to');
+            this.$el.addEventListener('transitionend',()=>{
+                let list = JSON.parse(localStorage.getItem("lists"));
+                let newList= [];
+                list.forEach(a=>{
+                    if(this.prdId != a.prd_id){
+                        newList.push(a);
+                    }
+                });
+                switch (this.$el.getAttribute('data-dis')) {
+                    case 'A':
+                        if(left.disArrA.length!=0){
+                        left.disArrA=left.disArrA.replace('A','')
+                        }
+                        break;
+                    case 'B':
+                        if(left.disArrB.length!=0){
+                        left.disArrB=left.disArrB.replace('B','')
+                        break;
+                        }
+                }
+                localStorage.setItem("lists", JSON.stringify(newList));
+                this.$destroy();
+                this.$el.parentNode.removeChild(this.$el);
+                right.total = right.total- (this.prdPrice * this.num);
+                right.proTotal-=this.num;
+
+                //購物車數量
+                let shoplist = JSON.parse(localStorage.getItem("lists"));
+                let shopIcon = document.querySelector('.shop');
+                let shopnum = 0;
+                shoplist.forEach(e => {
+                    shopnum += parseInt(e.num);
+                });
+                if (shopnum === 0) {
+                    return false;
+                } else if (!shopIcon.classList.contains('shopnum')) {
+                    shopIcon.setAttribute('data-content', shopnum);
+                    shopIcon.classList.add('shopnum');
+                } else {
+                    shopIcon.setAttribute('data-content', shopnum);
+                }
+            })
+        },
+    },
+    mounted() {
+        let list = JSON.parse(localStorage.getItem("lists"));
+        list.forEach((a,b)=>{
+            if(this.prdId===a.prd_id)this.num=a.num;
+        });
+        right.total+=parseInt(this.prdPrice)*this.num;
+    },
+});
